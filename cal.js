0cdbb512114589032f19cc07e0ff3b324485659a
LeoSHLin <47884553+aileolin1981@users.noreply.github.com>
2021-04-29 13:14:47 +0800

    Update cal.js

--- cal.js
+++ cal.js
@@ -18,7 +18,7 @@ async function classification_init() {
     maxPredictions = model.getTotalClasses();
 
     // Convenience function to setup a webcam
-    const flip = true;
+    const flip = false;
     webcam = new tmImage.Webcam(500, 500, flip);
     await webcam.setup();
     await webcam.play();
